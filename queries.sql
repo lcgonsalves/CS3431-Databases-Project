a21d388aa09b1574e8ca6706bd70cd7b37b8e1bd
Leo Gonsalves <lcgonsalves@wpi.edu>
2019-02-07 14:30:21 -0500

    added q6

--- queries.sql
+++ queries.sql
@@ -49,3 +49,12 @@ SELECT room_num
 FROM Equipment
 WHERE Equipment.serial_num = 'A01-02X';
 
+-- Q6: Report the employee who has access to the largest number of rooms. We need the
+-- employee ID, and the number of rooms (s)he can access.
+SELECT employee_id, max(CNT) as room_count
+FROM ( SELECT employee_id, count(room_id) as CNT
+	   FROM Permissions
+	   GROUP BY employee_id	)
+GROUP BY employee_id;
+
+
