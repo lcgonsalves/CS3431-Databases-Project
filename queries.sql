5ea715718e1d51896068335a7fa7b9f23e38a002
Leo Gonsalves <lcgonsalves@wpi.edu>
2019-02-07 14:16:18 -0500

    added q4

--- queries.sql
+++ queries.sql
@@ -30,3 +30,15 @@ WHERE E.id in
 SELECT patient as SSN, sum(Admission.cost * Admission.insurance_coverage)
 FROM Admission
 GROUP BY patient;
+
+
+
+-- Q4: Report the number of visits done for each patient, i.e., for each patient, 
+-- report the patient SSN, first and last names, and the count of 
+-- visits done by this patient.
+SELECT ssn, first_name, last_name, num_visits
+FROM Patient as P,
+	 ( SELECT patient, count(*) as num_visits -- count all visits --
+	   FROM Admission
+	   GROUP BY patient) as A
+WHERE A.patient = P.ssn;
