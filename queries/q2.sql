b3c6ad383def0a68dbd554ba0a0cfadb3a733007
Leo Gonsalves <lcgonsalves@wpi.edu>
2019-02-08 16:34:44 -0500

    fixed 2

--- queries/q2.sql
+++ queries/q2.sql
@@ -1,10 +1,10 @@
 -- Q2: For a given division manager (say, ID = 10), report all regular employees that are
 -- supervised by this manager. Display the employees ID, names, and salary.
 SELECT id, first_name, last_name, salary
-FROM Employee AS E
-WHERE E.id in
+FROM Employee 
+WHERE Employee.id in
 	( SELECT R.employee_id AS id
-	  FROM Regular AS R, DivisionalManager AS D
-	  WHERE R.divisional_manager_id = D.employee_id );
+	  FROM Regular, DivisionalManager
+	  WHERE Regular.divisional_manager_id = DivisionalManager.employee_id );
 
 	-- subquery should only have 1 column: id (which comes from Regular.employee_id) --
