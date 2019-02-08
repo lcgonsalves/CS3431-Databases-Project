d777b3e6ae7a2338049bea755f99d6b88ad1e607
Leo Gonsalves <lcgonsalves@wpi.edu>
2019-02-08 17:16:03 -0500

    minor changes

--- queries/q6.sql
+++ queries/q6.sql
@@ -1,7 +1,7 @@
 -- Q6: Report the employee who has access to the largest number of rooms. We need the
 -- employee ID, and the number of rooms (s)he can access.
-SELECT employee_id, max(CNT) as room_count
+SELECT employee_id, max(R1.CNT) as room_count
 FROM ( SELECT employee_id, count(room_id) as CNT
 	   FROM Permissions
-	   GROUP BY employee_id	)
+	   GROUP BY employee_id ) R1
 GROUP BY employee_id;
