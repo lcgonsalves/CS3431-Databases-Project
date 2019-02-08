5968e18e7aee5c5759cab092195882eb8dd67a10
aantaya1 <antayaalexander@gmail.com>
2019-02-08 18:25:33 -0500

    updated entries a little bit

--- queries/query12.sql
+++ queries/query12.sql
@@ -1,13 +1,11 @@
 -- Q12: Report the equipment types (only the ID) for which the hospital has purchased
 -- equipments (units) in both 2010 and 2011. Do not report duplication.
 
+SELECT type_id
+FROM Equipment
+WHERE year_of_purchase = 2010 OR year_of_purchase = 2011
+GROUP BY type_id;
 
 -- SELECT type_id
--- FROM Equipment
--- WHERE year_of_purchase = 2010 OR year_of_purchase = 2011
--- GROUP BY type_id;
-
-SELECT type_id
-FROM ( SELECT type_id, year_of_purchase FROM Equipment WHERE year_of_purchase = 2010 )
-WHERE year_of_purchase = 2011
-GROUP BY type_id;
+-- FROM ( SELECT type_id FROM Equipment WHERE year_of_purchase = 2010 )
+-- WHERE year_of_purchase = 2011;
