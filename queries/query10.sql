883730a641034190f0d9111ac1e65c025cf1b431
Leo Gonsalves <lcgonsalves@wpi.edu>
2019-02-08 18:47:46 -0500

    added new entries

--- queries/query10.sql
+++ queries/query10.sql
@@ -1,9 +1,9 @@
 -- Q10: Report the date of the coming future visit for patient with SSN = 111-22-3333.
 -- Note: This date should exist in the last (most recent) visit of that patient.
 SELECT future_visit_date
-FROM (SELECT id, max(check_in)
+FROM (SELECT patient, max(check_in)
 	  FROM Admission
 	  WHERE patient = '111-22-3333'
-	  GROUP BY id) R1,
+	  GROUP BY patient) R1,
 	 Admission
-WHERE Admission.id = R1.id;
+WHERE Admission.patient = R1.patient;
