ad03df016827c96269418f3229d7c68b857c60cf
jnpalmstrom <jnpalmstrom@gmail.com>
2019-02-22 12:13:43 -0500

    Working on final trigger

--- triggers.sql
+++ triggers.sql
@@ -22,14 +22,20 @@ CREATE OR REPLACE TRIGGER CalculateInsurance
 END;
 /
 
---
-CREATE OR REPLACE TRIGGER CalculateInsurance
-  AFTER INSERT OR UPDATE ON Admission
-  FOR EACH ROW
+-- Emergency Services are considered to be ICU and Stitching
+CREATE OR REPLACE TRIGGER EmergencyFutureDate
+  BEFORE INSERT ON Admission
+  DECLARE admissionType varchar2(100);
   BEGIN
 
+  SELECT exam.service INTO admissionType
+  FROM Examination exam
+  WHERE exam.admission_id = :new.id;
 
+  IF (admissionType = 'ICU' OR admissionType = 'Stitching') THEN
+    --:new.future_visit_date = ADD_MONTHS(:new.check_in, 2);
+    RAISE_APPLICATION_ERROR(-20004, 'You must leave a comment.');
+  END IF;
 
 END;
 /
-
