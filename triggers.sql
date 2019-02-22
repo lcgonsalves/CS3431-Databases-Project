af14bc4a8ffeddb435b677f22eec1c837560a8a0
jnpalmstrom <jnpalmstrom@gmail.com>
2019-02-22 18:42:07 -0500

    CS still sucks

--- triggers.sql
+++ triggers.sql
@@ -133,3 +133,40 @@ WHERE :new.type_id = ty.type_id;
 
 END;
 /
+
+set serveroutput on;
+CREATE OR REPLACE TRIGGER PatientLeavesHospital
+  AFTER UPDATE ON Admission
+  FOR EACH ROW
+  DECLARE
+    CURSOR cur_list IS
+      SELECT d.first_name, d.last_name, e.comments
+      FROM Doctor d, Examination e
+      WHERE e.doctor_id = d.id AND e.admission_id = :new.id;
+
+    curChoice cur_list%ROWTYPE;
+    fName varchar2(30);
+    lName varchar2(30);
+    address varchar2(50);
+
+  BEGIN
+
+  SELECT p.first_name, p.last_name, p.address
+  INTO fName, lName, address
+  FROM Patient p
+  WHERE p.ssn = :new.patient;
+
+  OPEN cur_list;
+  LOOP
+    FETCH cur_list INTO curChoice;
+    EXIT WHEN cur_list%NOTFOUND;
+    dbms_output.put_line(fName);
+    dbms_output.put_line(lName);
+    dbms_output.put_line(address);
+    dbms_output.put_line(curChoice.first_name);
+    dbms_output.put_line(curChoice.last_name);
+    dbms_output.put_line(curChoice.comments);
+  END LOOP;
+  CLOSE cur_list;
+END;
+/
